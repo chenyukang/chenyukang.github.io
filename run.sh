47e3bd59a1822580cf20c856a786efaab1268283
Yukang <moorekang@gmail.com>
2014-08-21 18:58:04 +0800

    update run.sh

---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+log="update"
+
+do_commit() {
+    cmd="git commit -a -m\"$log\""
+    echo $cmd
+    git add .;
+    git commit -am"$log"
+    git push;
+
+    simiki generate;
+    cd output;
+    git add .
+    git commit -am"$log";
+    git push
+    cd ../
+
+}
+
+while [ $# -gt 0 ]
+do
+    case $1 in
+        -commit |-u) shift; log=$1; do_commit; exit 0;;
+    esac
+    shift
+done
